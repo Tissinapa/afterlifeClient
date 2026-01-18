00d4b37a40677b75cbdebbb356248ac17f63c9df
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2026-01-18 16:53:31 +0200

    Feat: departure date picker to store page

--- src/app/public/feature/store/store.ts
+++ src/app/public/feature/store/store.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-store',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './store.html',
   styleUrl: './store.css',
 })
 export class Store {
-
+  minDate = new Date();
+  departureDate?: Date;
 }
