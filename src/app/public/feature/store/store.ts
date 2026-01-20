6c1004710fee91192a2394c62fcbbdd12b65f2b4
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2026-01-20 17:18:24 +0200

    Feat: seat class and seat matrixes

--- src/app/public/feature/store/store.ts
+++ src/app/public/feature/store/store.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {ChangeDetectionStrategy} from '@angular/core';
 
 
-// Destination is enum, does this need to be changed?. Kysympähän vaan tulevaisuutta varten itseltäni?
-interface Destination {
+// Destination is enum in bacakend, does this need to be changed?. Kysympähän vaan tulevaisuutta varten itseltäni!?
+/* interface Destination {
   name: String;
   viewValue: String;
+} */
+
+enum DestinationEnum{
+  HEAVEN = "HEAVEN",
+  HELL = "HELL"
 }
 
 @Component({
@@ -25,18 +33,27 @@ interface Destination {
     MatNativeDateModule,
     FormsModule,
     CommonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   templateUrl: './store.html',
   styleUrl: './store.css',
+  
 })
 export class Store {
   minDate = new Date();
   departureDate?: Date;
 
-  destinations: Destination[] = [
-    {name: 'heaven', viewValue: 'Heaven'},
-    {name: 'hell', viewValue: 'Hell'},
+  selectedDestination?: DestinationEnum;
+  selectedSeat?: number;
+
+  destinations = [
+    { name: DestinationEnum.HEAVEN, viewValue: 'Heaven' },
+    { name: DestinationEnum.HELL, viewValue: 'Hell' },
   ];
+
+  seats = Array.from({ length: 30 }, (_, i) => i + 1); // [1..30]
+
 }
 
